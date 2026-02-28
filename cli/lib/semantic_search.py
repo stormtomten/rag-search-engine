302c3d2b7f8c06759bde7d63424ad630fef370e7
stormtomten <80994025+stormtomten@users.noreply.github.com>
2026-02-28 02:30:55 +0100

    Ch.4 L3 Embedding Models

---
+++ cli/lib/semantic_search.py
@@ -0,0 +1,13 @@
+from sentence_transformers import SentenceTransformer
+
+
+class SemanticSearch:
+    def __init__(self):
+        self.model = SentenceTransformer("all-MiniLM-L6-V2")
+
+
+def verify_model():
+    model = SemanticSearch()
+
+    print(f"Model loaded: {model.model}")
+    print(f"Max sequence length: {model.model.max_seq_length}")
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,12 +2,20 @@
 
 import argparse
 
+from lib.semantic_search import verify_model
+
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
-    args = parser.parse_args()
+    subparsers = parser.add_subparsers(dest="command", help="Available Commands")
 
+    verify_parser = subparsers.add_parser("verify", help="Verify Model")
+
+    args = parser.parse_args()
     match args.command:
+        case "verify":
+            verify_model()
+
         case _:
             parser.print_help()
 
