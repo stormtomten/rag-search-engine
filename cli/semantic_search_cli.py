e8db5144ec77e9c9083867f07293e425b831b76e
stormtomten <80994025+stormtomten@users.noreply.github.com>
2026-02-28 16:43:50 +0100

    Ch.4 L10 Generating Text Embeddings

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-from lib.semantic_search import verify_model
+from lib.semantic_search import embed_text, verify_embeddings, verify_model
 
 
 def main():
@@ -10,11 +10,21 @@ def main():
     subparsers = parser.add_subparsers(dest="command", help="Available Commands")
 
     verify_parser = subparsers.add_parser("verify", help="Verify Model")
+    verify_embeddings_parser = subparsers.add_parser(
+        "verify_embeddings", help="Verify Embeddings"
+    )
+    embed_parser = subparsers.add_parser("embed_text", help="Embed Text")
+    embed_parser.add_argument("text", type=str, help="Text to Embed")
 
     args = parser.parse_args()
     match args.command:
         case "verify":
             verify_model()
+
+        case "embed_text":
+            embed_text(args.text)
+        case "verify_embeddings":
+            verify_embeddings()
 
         case _:
             parser.print_help()
